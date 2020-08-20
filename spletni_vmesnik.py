f00f581b122025b67d28d6d8fd535d797d0c41b4
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2020-08-20 14:36:22 +0200

    Zahteva za velikost polja od uporabnika.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,7 +1,23 @@
 import bottle
 
+x = 0
+y = 0
+
 @bottle.get("/")
 def GlavnaStran():
     return bottle.template("glavna_stran.html")
+
+@bottle.post("/")
+def GlavnaStranPost():
+    x = bottle.request.forms.get("x")
+    y = bottle.request.forms.get("y")
+    print(x)
+    print(y)
+    bottle.redirect("/igra")
+
+@bottle.get("/igra")
+def Igra():
+    return str(x) + " " + str(y)
+
 
 bottle.run()
